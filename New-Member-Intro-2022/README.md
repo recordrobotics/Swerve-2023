c4a19a9049f1fde7e0f99d14a0247272e80212cd
Person-26 <95951869+Person-26@users.noreply.github.com>
2023-06-27 22:59:48 -0400

    Add files via upload

---
+++ New-Member-Intro-2022/README.md
@@ -0,0 +1,2 @@
+# New Member Intro Project
+## 2022-2023
---
+++ New-Member-Intro-2022/src/main/java/org/recordrobotics/munchkin/RobotMap.java
@@ -0,0 +1,19 @@
+package org.recordrobotics.munchkin;
+
+/**
+ * Hardware ports for computer and robot
+ */
+public class RobotMap {
+
+	/**
+	 * Control ports (PC USB)
+	 */
+	public class Control {
+		// LegacyControl
+		public static final int LEGACY_GAMEPAD = 0;
+
+		// DoubleControl
+		public static final int DOUBLE_GAMEPAD_1 = 0;
+		public static final int DOUBLE_GAMEPAD_2 = 1;
+	}
+}
