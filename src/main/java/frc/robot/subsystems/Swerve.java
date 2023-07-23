d31e0789137f9d339a11eca1a6f6821327353b22
Person-26 <95951869+Person-26@users.noreply.github.com>
2023-07-23 14:22:34 -0400

    format

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -6,14 +6,10 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.kinematics.*;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
@@ -23,16 +19,18 @@ public class Swerve extends SubsystemBase {
   public static final double GEAR_RATIO = 1;
 
   // TODO change to correct motor
-  private TalonFX LFSmotor = new TalonFX(RobotMap.swerve.LFS_MOTOR_PORT);
-  private TalonFX LBSmotor = new TalonFX(RobotMap.swerve.LBS_MOTOR_PORT);
-  private TalonFX RFSmotor = new TalonFX(RobotMap.swerve.RFS_MOTOR_PORT);
-  private TalonFX RBSmotor = new TalonFX(RobotMap.swerve.RBS_MOTOR_PORT);
-
-  private TalonFX LFDmotor = new TalonFX(RobotMap.swerve.LFD_MOTOR_PORT);
-  private TalonFX LBDmotor = new TalonFX(RobotMap.swerve.LBD_MOTOR_PORT);
-  private TalonFX RFDmotor = new TalonFX(RobotMap.swerve.RFD_MOTOR_PORT);
-  private TalonFX RBDmotor = new TalonFX(RobotMap.swerve.RBD_MOTOR_PORT);
-
+  private TalonFX[] speedMotors = {
+      new TalonFX(RobotMap.swerve.LFS_MOTOR_PORT),
+      new TalonFX(RobotMap.swerve.LBS_MOTOR_PORT),
+      new TalonFX(RobotMap.swerve.RFS_MOTOR_PORT),
+      new TalonFX(RobotMap.swerve.RBS_MOTOR_PORT)
+  };
+  private TalonFX[] directionMotors = {
+      new TalonFX(RobotMap.swerve.LFD_MOTOR_PORT),
+      new TalonFX(RobotMap.swerve.LBD_MOTOR_PORT),
+      new TalonFX(RobotMap.swerve.RFD_MOTOR_PORT),
+      new TalonFX(RobotMap.swerve.RBD_MOTOR_PORT)
+  };
   private final double moduleWidth = 0.762;
   private final double moduleLength = 0.762;
 
@@ -46,54 +44,42 @@ public class Swerve extends SubsystemBase {
 
   private SwerveModuleState[] MOD_TARGETS;
 
-  AHRS _nav = new AHRS(SerialPort.Port.kUSB1);
-  SwerveDriveOdometry m_Odometry = new SwerveDriveOdometry(kinematics, _nav.getRotation2d());
+  public AHRS _nav = new AHRS(SerialPort.Port.kUSB1);
+  private static final SwerveModulePosition[] startPos = {
+      new SwerveModulePosition(),
+      new SwerveModulePosition(),
+      new SwerveModulePosition(),
+      new SwerveModulePosition()
+  };
+  SwerveDriveOdometry m_Odometry = new SwerveDriveOdometry(kinematics, _nav.getRotation2d(), startPos);
 
   ChassisSpeeds target;
 
-  private static final TalonFXConfiguration swerveConfig = new TalonFXConfiguration();
-
   public Swerve() {
-    LFSmotor.configAllSettings(swerveConfig);
-    LBSmotor.configAllSettings(swerveConfig);
-    RFSmotor.configAllSettings(swerveConfig);
-    RBSmotor.configAllSettings(swerveConfig);
+    speedMotors[0].set(ControlMode.Velocity, 0);
+    speedMotors[1].set(ControlMode.Velocity, 0);
+    speedMotors[2].set(ControlMode.Velocity, 0);
+    speedMotors[3].set(ControlMode.Velocity, 0);
 
-    LFDmotor.configAllSettings(swerveConfig);
-    LBDmotor.configAllSettings(swerveConfig);
-    RFDmotor.configAllSettings(swerveConfig);
-    RBDmotor.configAllSettings(swerveConfig);
-
-    LFSmotor.set(ControlMode.Velocity, 0);
-    LBSmotor.set(ControlMode.Velocity, 0);
-    RFSmotor.set(ControlMode.Velocity, 0);
-    RBSmotor.set(ControlMode.Velocity, 0);
-
-    LFDmotor.set(ControlMode.Position, 0);
-    LBDmotor.set(ControlMode.Position, 0);
-    RFDmotor.set(ControlMode.Position, 0);
-    RBDmotor.set(ControlMode.Position, 0);
+    directionMotors[0].set(ControlMode.Position, 0);
+    directionMotors[0].set(ControlMode.Position, 0);
+    directionMotors[0].set(ControlMode.Position, 0);
+    directionMotors[0].set(ControlMode.Position, 0);
 
   }
 
-  public SwerveModuleState LFState() {
-    return new SwerveModuleState(LFSmotor.getSelectedSensorVelocity() * GEAR_RATIO,
-        new Rotation2d(2 * Math.PI * LFDmotor.getSelectedSensorPosition() * GEAR_RATIO));
-  }
-
-  public SwerveModuleState LBState() {
-    return new SwerveModuleState(LBSmotor.getSelectedSensorVelocity() * GEAR_RATIO,
-        new Rotation2d(2 * Math.PI * LBDmotor.getSelectedSensorPosition() * GEAR_RATIO));
-  }
-
-  public SwerveModuleState RFState() {
-    return new SwerveModuleState(RFSmotor.getSelectedSensorVelocity() * GEAR_RATIO,
-        new Rotation2d(2 * Math.PI * RFDmotor.getSelectedSensorPosition() * GEAR_RATIO));
-  }
-
-  public SwerveModuleState RBState() {
-    return new SwerveModuleState(RBSmotor.getSelectedSensorVelocity(),
-        new Rotation2d(2 * Math.PI * RBDmotor.getSelectedSensorPosition() * GEAR_RATIO));
+  public SwerveModuleState[] modState() {
+    SwerveModuleState[] LFState = {
+        new SwerveModuleState(speedMotors[0].getSelectedSensorVelocity() * GEAR_RATIO,
+            new Rotation2d(2 * Math.PI * directionMotors[0].getSelectedSensorPosition() * GEAR_RATIO)),
+        new SwerveModuleState(speedMotors[1].getSelectedSensorVelocity() * GEAR_RATIO,
+            new Rotation2d(2 * Math.PI * directionMotors[1].getSelectedSensorPosition() * GEAR_RATIO)),
+        new SwerveModuleState(speedMotors[2].getSelectedSensorVelocity() * GEAR_RATIO,
+            new Rotation2d(2 * Math.PI * directionMotors[2].getSelectedSensorPosition() * GEAR_RATIO)),
+        new SwerveModuleState(speedMotors[3].getSelectedSensorVelocity(),
+            new Rotation2d(2 * Math.PI * directionMotors[3].getSelectedSensorPosition() * GEAR_RATIO))
+    };
+    return LFState;
   }
 
   public void setTarget(ChassisSpeeds _target) {
@@ -104,20 +90,20 @@ public class Swerve extends SubsystemBase {
   public void periodic() {
     MOD_TARGETS = kinematics.toSwerveModuleStates(target);
 
-    SwerveModuleState.optimize(MOD_TARGETS[0], LFState().angle);
-    SwerveModuleState.optimize(MOD_TARGETS[1], LBState().angle);
-    SwerveModuleState.optimize(MOD_TARGETS[2], RFState().angle);
-    SwerveModuleState.optimize(MOD_TARGETS[3], RBState().angle);
+    SwerveModuleState.optimize(MOD_TARGETS[0], modState()[0].angle);
+    SwerveModuleState.optimize(MOD_TARGETS[1], modState()[1].angle);
+    SwerveModuleState.optimize(MOD_TARGETS[2], modState()[2].angle);
+    SwerveModuleState.optimize(MOD_TARGETS[3], modState()[3].angle);
 
-    LFSmotor.set(ControlMode.Velocity, MOD_TARGETS[0].speedMetersPerSecond);
-    LBSmotor.set(ControlMode.Velocity, MOD_TARGETS[1].speedMetersPerSecond);
-    RFSmotor.set(ControlMode.Velocity, MOD_TARGETS[2].speedMetersPerSecond);
-    RBSmotor.set(ControlMode.Velocity, MOD_TARGETS[3].speedMetersPerSecond);
+    speedMotors[0].set(ControlMode.Velocity, MOD_TARGETS[0].speedMetersPerSecond);
+    speedMotors[1].set(ControlMode.Velocity, MOD_TARGETS[1].speedMetersPerSecond);
+    speedMotors[2].set(ControlMode.Velocity, MOD_TARGETS[2].speedMetersPerSecond);
+    speedMotors[3].set(ControlMode.Velocity, MOD_TARGETS[3].speedMetersPerSecond);
 
-    LFDmotor.set(ControlMode.Position, MOD_TARGETS[0].angle.getDegrees() * 256 / 45);
-    LBDmotor.set(ControlMode.Position, MOD_TARGETS[1].angle.getDegrees() * 256 / 45);
-    RFDmotor.set(ControlMode.Position, MOD_TARGETS[2].angle.getDegrees() * 256 / 45);
-    RBDmotor.set(ControlMode.Position, MOD_TARGETS[3].angle.getDegrees() * 256 / 45);
+    directionMotors[0].set(ControlMode.Position, MOD_TARGETS[0].angle.getDegrees() * 256 / 45);
+    directionMotors[0].set(ControlMode.Position, MOD_TARGETS[1].angle.getDegrees() * 256 / 45);
+    directionMotors[0].set(ControlMode.Position, MOD_TARGETS[2].angle.getDegrees() * 256 / 45);
+    directionMotors[0].set(ControlMode.Position, MOD_TARGETS[3].angle.getDegrees() * 256 / 45);
   }
 
   @Override
